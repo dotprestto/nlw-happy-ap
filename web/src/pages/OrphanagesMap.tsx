ff7dd754acd0f1f2fef906d91f30825aed33e6d0
Allan Presotto <allanpresottoifpr@gmail.com>
2020-10-13 14:50:28 -0300

    pagina do mapa de orfanatos

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+
+import '../styles/pages/orphanages-map.css'
+import mapMarkerImg from '../images/map-marker.svg'
 
 export default function OrphanagesMap() {
-    return(
-        <h1>Hello World</h1>
+    return (
+    <div id="page-map">
+        <aside>
+            <header>
+                <img src={mapMarkerImg} alt="Happy"/>
+                <h2>Escolha um orfanato no mapa</h2>
+                <p>Muitas crianças estão esperando a sua visita :)</p>
+            </header>
+            <footer>
+                <strong>Foz do Iguaçu</strong>
+                <span>Paraná</span>
+            </footer>
+        </aside>
+        
+        <Map 
+            center={[-25.5172661, -54.6171757]} 
+            zoom={12} 
+            style={{
+                width: "100%", height: "100%"
+            }}
+        >
+            <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </Map>
+
+        <Link to="" className="create-orphanage">
+            <FiPlus size={32} color="#FFF"/>
+        </Link>
+    </div>
     )
 }
