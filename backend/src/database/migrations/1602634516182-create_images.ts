2e5a8ea8f65a23de66a27688ed5f156d118a18d3
Allan Presotto <allanpresottoifpr@gmail.com>
2020-10-13 21:38:06 -0300

    upload de imagem

--- backend/src/database/migrations/1602634516182-create_images.ts
+++ backend/src/database/migrations/1602634516182-create_images.ts
@@ -1,8 +1,39 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createImages1602634516182 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'images',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true, 
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'path',
+                    type: 'varchar',
+                },
+                {
+                    name: 'orphanage_id',
+                    type: 'integer',
+                },
+            ],
+            foreignKeys: [
+                {
+                    name: 'ImageOrphanage',
+                    columnNames: ['orphanage_id'],
+                    referencedTableName: 'orphanages',
+                    referencedColumnNames: ['id'],
+                    onUpdate: 'CASCADE',
+                    onDelete: ''
+                }
+            ]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
