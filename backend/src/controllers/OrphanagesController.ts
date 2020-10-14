9e838f39281b74e583e4913de7bf5a13eb38935f
Allan Presotto <allanpresottoifpr@gmail.com>
2020-10-14 16:35:21 -0300

    validaçao de dados e erros de validaçao com Yup

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanage'
 import { Request, Response } from 'express'
 import orphanageView from '../views/orphanage_views'
+import * as Yup from 'yup'
 
 export default {
     async index (req: Request, res: Response) {
@@ -37,9 +38,8 @@ export default {
 
         const requestImages = req.files as Express.Multer.File[]
         const images = requestImages.map(image => { return { path: image.filename } })
-    
-        const orphanagesRepository = getRepository(Orphanage)
-        const orphanage = orphanagesRepository.create({
+
+        const data = {
             name, 
             latitude, 
             longitude, 
@@ -48,7 +48,27 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(), 
+            latitude: Yup.number().required(), 
+            longitude: Yup.number().required(), 
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         })
+
+        await schema.validate(data, { abortEarly: false })
+    
+        const orphanagesRepository = getRepository(Orphanage)
+        const orphanage = orphanagesRepository.create(data)
     
         await orphanagesRepository.save(orphanage)
     
