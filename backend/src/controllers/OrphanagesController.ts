6d4b68d350f03c4faa56eb63d1bace78a8d0bd09
Allan Presotto <allanpresottoifpr@gmail.com>
2020-10-17 13:03:58 -0300

    criaçao de dados

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -46,7 +46,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: open_on_weekends === "true",
             images
         }
 
@@ -64,7 +64,7 @@ export default {
                 })
             )
         })
-
+       
         await schema.validate(data, { abortEarly: false })
     
         const orphanagesRepository = getRepository(Orphanage)
